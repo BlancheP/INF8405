8e4123cec19a6a9851ee57aac8c4d25ad83261ae
Blanche <blanche012006@yahoo.ca>
2017-01-25 12:06:55 -0500

    Ajout de lactivite pour les regles du jeu et ajout de lactivite pour choisir le niveau

--- Match3/app/src/main/java/tp1/inf8405/match3/MainActivity.java
+++ Match3/app/src/main/java/tp1/inf8405/match3/MainActivity.java
@@ -1,5 +1,6 @@
 package tp1.inf8405.match3;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,5 +16,15 @@ public class MainActivity extends AppCompatActivity {
     public void onQuit(View view){
         finish();
         System.exit(1);
+    }
+
+    public void clickRules(View view){
+        Intent intent = new Intent(this, RulesActivity.class);
+        startActivity(intent);
+    }
+
+    public void clickPlay(View view){
+        Intent intent = new Intent(this, ChooseLevelActivity.class);
+        startActivity(intent);
     }
 }
