ef350a871a65b49624f7d303f157205709b7e7aa
Paiement Blanche <Paiement Blanche>
2017-01-25 10:21:56 -0500

    Creation des activites pour jouer et les regles du jeu

---
+++ Match3/app/src/main/java/tp1/inf8405/match3/RulesActivity.java
@@ -0,0 +1,13 @@
+package tp1.inf8405.match3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RulesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rules);
+    }
+}
