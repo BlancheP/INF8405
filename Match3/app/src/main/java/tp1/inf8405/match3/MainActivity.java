33c4ac24d5698b66451214c32e911ff3b2b0137b
Paiement Blanche <Paiement Blanche>
2017-01-25 11:23:43 -0500

    Ajout de la fonctionnalite pour quitter lapplication

--- Match3/app/src/main/java/tp1/inf8405/match3/MainActivity.java
+++ Match3/app/src/main/java/tp1/inf8405/match3/MainActivity.java
@@ -2,6 +2,7 @@ package tp1.inf8405.match3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onQuit(View view){
+        finish();
+        System.exit(1);
     }
 }
