b64e5c029c490ccc4a54229dd853e77070561158
Alexis <alexis.st-onge@polymtl.ca>
2017-03-02 16:27:02 -0500

    ajout de firebase

--- TP2/OrgEvents/app/src/main/java/com/example/blanche/orgevents/MainActivity.java
+++ TP2/OrgEvents/app/src/main/java/com/example/blanche/orgevents/MainActivity.java
@@ -3,11 +3,17 @@ package com.example.blanche.orgevents;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
 public class MainActivity extends AppCompatActivity {
+
+    private StorageReference mStorageRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mStorageRef = FirebaseStorage.getInstance().getReference();
     }
 }
