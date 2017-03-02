bbd41ae15e6c51ac410109be8c281c90c45e528a
Blanche <blanche012006@yahoo.ca>
2017-03-02 13:13:35 -0500

    On peut prendre une photo et la mettre dans une ImageView + ajout d'une activity pour le login

--- TP2/OrgEvents/app/src/main/java/com/example/blanche/orgevents/MainActivity.java
+++ TP2/OrgEvents/app/src/main/java/com/example/blanche/orgevents/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.blanche.orgevents;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        dispatchTakePictureIntent();
+    }
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap)extras.get("data");
+            ImageView v = (ImageView)findViewById(R.id.photoView);
+            v.setImageBitmap(imageBitmap);
+        }
     }
 }
