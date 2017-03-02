6d6d9aff28f984cf87e5370a1d149203015a97eb
Blanche <blanche012006@yahoo.ca>
2017-03-02 14:06:10 -0500

    L'application ouvre sur l'activity de login

--- TP2/OrgEvents/app/src/main/java/com/example/blanche/orgevents/MainActivity.java
+++ TP2/OrgEvents/app/src/main/java/com/example/blanche/orgevents/MainActivity.java
@@ -12,8 +12,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        dispatchTakePictureIntent();
+        setContentView(R.layout.activity_login);
+        //dispatchTakePictureIntent();
     }
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
