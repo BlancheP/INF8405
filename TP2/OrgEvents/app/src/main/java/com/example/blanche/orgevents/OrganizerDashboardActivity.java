f73819032059d60caa2080563c734a0ad0671ce2
Alexis <alexis.st-onge@polymtl.ca>
2017-03-16 18:47:59 -0400

    started to work on buttons for vote and create event

--- TP2/OrgEvents/app/src/main/java/com/example/blanche/orgevents/OrganizerDashboardActivity.java
+++ TP2/OrgEvents/app/src/main/java/com/example/blanche/orgevents/OrganizerDashboardActivity.java
@@ -5,7 +5,10 @@ import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationRequest;
@@ -23,6 +26,16 @@ public class OrganizerDashboardActivity extends ActivityWithMenu{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_organizerdashboard);
+
+        Button bCreateEvent = (Button) findViewById(R.id.bCreateEvent);
+        Button bVote = (Button) findViewById(R.id.bVote);
+
+        bVote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View w){
+                AlertDialog.Builder voteBuilder = new AlertDialog.Builder(OrganizerDashboardActivity.this);
+            }
+        });
     }
 
 }
