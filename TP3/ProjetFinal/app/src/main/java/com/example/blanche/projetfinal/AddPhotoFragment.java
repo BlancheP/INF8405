3745358bcf4a04cf0688596f5ba91cfaa90ac28d
BlancheP <blanche012006@yahoo.ca>
2017-04-17 13:35:57 -0400

    modification de l'icone + modification du fragment pour ajouter une photo

--- TP3/ProjetFinal/app/src/main/java/com/example/blanche/projetfinal/AddPhotoFragment.java
+++ TP3/ProjetFinal/app/src/main/java/com/example/blanche/projetfinal/AddPhotoFragment.java
@@ -1,14 +1,25 @@
 package com.example.blanche.projetfinal;
 
+import android.*;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 
 public class AddPhotoFragment extends Fragment {
 
+    private final int REQUEST_IMAGE_CAPTURE = 1;
+    private final int MY_CAMERA_REQUEST_CODE = 100;
 
     public AddPhotoFragment() {
         // Required empty public constructor
@@ -18,5 +29,58 @@ public class AddPhotoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_add_photo, container, false);
+    }
+
+    @Override
+    public void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (checkIfAlreadyHavePermission())
+            dispatchTakePictureIntent();
+        else
+            requestForSpecificPermission();
+    }
+
+    private boolean checkIfAlreadyHavePermission() {
+        int result = ContextCompat.checkSelfPermission(getActivity(), android.Manifest.permission.CAMERA);
+        if (result == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private void requestForSpecificPermission() {
+        ActivityCompat.requestPermissions(getActivity(), new String[] {android.Manifest.permission.CAMERA}, MY_CAMERA_REQUEST_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case MY_CAMERA_REQUEST_CODE:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    dispatchTakePictureIntent();
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+       /* bitmap.eraseColor(Color.TRANSPARENT);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            bitmap = (Bitmap)extras.get("data");
+            ImageButton b = (ImageButton) findViewById(R.id.bPhoto);
+            bitmap = Bitmap.createScaledBitmap(bitmap, b.getWidth(), b.getHeight(), true);
+            b.setImageBitmap(bitmap);
+        }*/
     }
 }
