d5cbdb7b08fc0529b4fe78b4d9b936912a337345
BlancheP <blanche012006@yahoo.ca>
2017-04-17 13:15:37 -0400

    ajout de la toolbar en haut de l'application

---
+++ TP3/ProjetFinal/app/src/main/java/com/example/blanche/projetfinal/AddPhotoFragment.java
@@ -0,0 +1,22 @@
+package com.example.blanche.projetfinal;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class AddPhotoFragment extends Fragment {
+
+
+    public AddPhotoFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_add_photo, container, false);
+    }
+}
--- TP3/ProjetFinal/app/src/main/java/com/example/blanche/projetfinal/MainActivity.java
+++ TP3/ProjetFinal/app/src/main/java/com/example/blanche/projetfinal/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.blanche.projetfinal;
 
-import android.content.SharedPreferences;
+import android.content.Intent;
+import android.media.audiofx.BassBoost;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends AppCompatActivity {
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -29,8 +36,6 @@ public class MainActivity extends FragmentActivity {
                     fragmentTransaction.replace(R.id.fragment_container, new MapFragment());
                     fragmentTransaction.commit();
                     return true;
-                case R.id.navigation_settings:
-                    return true;
             }
             return false;
         }
@@ -48,5 +53,43 @@ public class MainActivity extends FragmentActivity {
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
         fragmentTransaction.replace(R.id.fragment_container, new DashboardFragment());
         fragmentTransaction.commit();
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+        ImageView addPhoto = (ImageView) findViewById(R.id.addPhoto);
+        addPhoto.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_container, new AddPhotoFragment());
+                fragmentTransaction.commit();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.settings:
+                Intent goToSettings = new Intent(this, SettingsActivity.class);
+                startActivity(goToSettings);
+                return true;
+            case R.id.logout:
+                Intent goToLogin = new Intent(this, LoginActivity.class);
+                PreferencesManager pm = DatabaseManager.getPreferencesManager();
+                pm.removeCurrentUser();
+                startActivity(goToLogin);
+                finishAffinity();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
