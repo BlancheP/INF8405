1fe7930175af189d178f80d4b7aa3e6d0f08e6a8
Alain Tran-Dang <alain.trandang@Alains-MacBook-Pro.local>
2017-04-18 16:20:26 -0400

    affichage map de base fonctionnel

--- TP3/ProjetFinal/app/src/main/java/com/example/blanche/projetfinal/MainActivity.java
+++ TP3/ProjetFinal/app/src/main/java/com/example/blanche/projetfinal/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.blanche.projetfinal;
 
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
