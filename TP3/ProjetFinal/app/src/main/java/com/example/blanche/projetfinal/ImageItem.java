a06bc9a210c3ee757305664c2ac0bcf67c62f403
Alexis <alexis.st-onge@polymtl.ca>
2017-04-25 19:28:15 -0400

    forgot some files for previous commit

---
+++ TP3/ProjetFinal/app/src/main/java/com/example/blanche/projetfinal/ImageItem.java
@@ -0,0 +1,34 @@
+package com.example.blanche.projetfinal;
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by Alexis on 25/04/2017.
+ */
+
+public class ImageItem {
+    private Bitmap image;
+    private String title;
+
+    public ImageItem(Bitmap image, String title) {
+        super();
+        this.image = image;
+        this.title = title;
+    }
+
+    public Bitmap getImage() {
+        return image;
+    }
+
+    public void setImage(Bitmap image) {
+        this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
